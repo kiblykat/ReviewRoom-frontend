b4f9e9915d4d33adc4f516a1428b93c1a0b00443
nhkhai <nhkhai@gmail.com>
2024-05-13 15:44:22 +0800

    Added setup for React test tools.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react({ fastRefresh: false })],
+  worker: {
+    plugins: [react()],
+  },
+  test: {
+    // Add the line below to add Jsdom to Vite.
+    environment: "jsdom",
+    // Make all imports from Vitest global so that we don’t manually import(e.g. expect, describe, it) in each test file.
+    globals: true,
+    setupFiles: "./tests/setup.js", // assuming the test folder is in the root of our project
+  },
+});
