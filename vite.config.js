c8a5551d04888be8746132818a5c6113e71f2c60
Ng Heng Khai <nhkhai@gmail.com>
2024-05-14 02:19:01 +0800

    Updated to serve the content using a simple http server instead.

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,17 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({ fastRefresh: false })],
-  worker: {
-    plugins: [react()],
+  base: "/",
+  plugins: [react()],
+  preview: {
+    port: 8080,
+    strictPort: true,
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "https://0.0.0.0:8080",
   },
   test: {
     // Add the line below to add Jsdom to Vite.
