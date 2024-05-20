46ddcc99c85abf5c0f3b4be4ee263bdbf9f7db27
izzat <izzatfadzlon@gmail.com>
2024-05-21 00:15:12 +0800

    update useeEffect hook

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -59,7 +59,9 @@ export default function Dashboard() {
     }
   };
 
-  useEffect(() => customersLoader, []);
+  useEffect(() => {
+    customersLoader();
+  }, []);
 
   return (
     <SimpleGrid spacing={10} minChildWidth="250px">
